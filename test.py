96648c71cd4b72c54bc87d6e47c966d0c09b2e10
Namrata-nts <159518984+Namrata-nts@users.noreply.github.com>
2024-06-21 15:58:58 +0530

    changed test.py through desktop

--- test.py
+++ test.py
@@ -2,3 +2,11 @@ a = 2
 b = 5
 print("The sum of a and b is: ", a+b)
 print("Step1: cd path\nStep2: git add\nStep3: git commit -m 'comment'\nStep4: git push origin <branch_name>")
+# 1. navigate to folder -> cd
+#2. git init
+#3. git add <. or file_name>
+#4. git commit -m "comment"
+#5. create repo in web page -> copy url
+#6. git remote add origin paste url ------------ git remote remove origin
+#7. git push -u origin branch_name
+print("Check if changes apply")
